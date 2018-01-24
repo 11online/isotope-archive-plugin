25d7b4d90363ef98314d775db0cf71fb7e7f42fa
Monica Alvarez <mmalvar13@gmail.com>
2018-01-23 17:16:05 -0700

    wrapped inputs in div for structure

--- views/settings.php
+++ views/settings.php
@@ -1,55 +1,58 @@
 <?php
 
 
-
-
-
-
-
-
-
 ?>
 
 
 <div class="wrap">
-    <h1>Your Plugin Page Title</h1>
+    <h1>Isotope.js Archive Plugin</h1>
 
     <form method="post" action="options.php">
 
 
         <label for="filtering_post_type">Select Post Type</label>
-        <select id="filtering_post_type" name="filtering_post_type">
-            <?php
-            foreach ( get_post_types( '', 'names' ) as $post_type ) {
-                echo '<option value="' . $post_type .'">' . $post_type . '</option>';
-            }
-            ?>
+        <div>
+            <select id="filtering_post_type" name="filtering_post_type">
+                <?php
+                foreach (get_post_types('', 'names') as $post_type) {
+                    echo '<option value="' . $post_type . '">' . $post_type . '</option>';
+                }
+                ?>
 
-        </select>
-
+            </select>
+        </div>
 
         <label for="filtering_columns">Number Of Columns</label>
-        <select name="filtering_columns" id="filtering_columns">
-            <option value="1">1</option>
-            <option value="2">2</option>
-            <option value="3">3</option>
-        </select>
+        <div>
+            <select name="filtering_columns" id="filtering_columns">
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+            </select>
+        </div>
+
 
         <label for="filtering_taxonomy">Taxonomy</label>
-        <select name="filtering_taxonomy" id="filtering_taxonomy">
-<?php
-            $taxonomies = get_taxonomies();
-            foreach ( $taxonomies as $taxonomy ) {
-            echo '<option value="'. $taxonomy . '">' . $taxonomy . '</option>';
-            }
-            ?>
-        </select>
+        <div>
+            <select name="filtering_taxonomy" id="filtering_taxonomy">
+                <?php
+                $taxonomies = get_taxonomies();
+                foreach ($taxonomies as $taxonomy) {
+                    echo '<option value="' . $taxonomy . '">' . $taxonomy . '</option>';
+                }
+                ?>
+            </select>
+        </div>
 
         <label for="filtering_color">Color</label>
-        <input type="text" id="filtering_color" value="#bada55" class="my-color-field" data-default-color="#effeff" />
+        <div>
+            <input type="text" id="filtering_color" value="#bada55" class="my-color-field"
+                   data-default-color="#effeff"/>
+        </div>
 
-
-        <button type="submit" id="submit">Submit</button>
+        <div>
+            <button type="submit" id="submit">Submit</button>
+        </div>
 
     </form>
 
